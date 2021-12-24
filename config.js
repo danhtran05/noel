0b8740f6ee2b4445ceeb356ef2684bcfe04a9e6f
duongduong <93088754+duongduong1106@users.noreply.github.com>
2021-12-25 00:39:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc noel vui vẻ';
 const titleCard = 'Người con gái anh yêu';
 const contentCard = 'Chúc em có một ngày noel hạnh phúc và vui vẻ bên gia đình của mình';
-const giftImage = 'vy.png';
+const giftImage = 'vy1.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
